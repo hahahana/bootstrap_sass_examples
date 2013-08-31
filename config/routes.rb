3d3633de2b99a448421cfcf3c5338a3962640af8
Hana Wang <h.wang081@gmail.com>
2013-08-30 19:31:27 -0500

    Add justified nav

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ BootstrapSassExamples::Application.routes.draw do
   get 'grid', to: 'bootstrap_examples#grid'
   get 'jumbotron', to: 'bootstrap_examples#jumbotron'
   get 'jumbotron_narrow', to: 'bootstrap_examples#jumbotron_narrow'
+  get 'justified_nav', to: 'bootstrap_examples#justified_nav'
   root 'bootstrap_examples#home'
 end
  
