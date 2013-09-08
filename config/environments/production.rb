1058ac7a7dc0635b70b8a0215d49c0d780fbdb58
Hana Wang <hana@molecule.io>
2013-09-07 23:35:32 -0500

    Only load example js when needed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,8 +60,10 @@ BootstrapSassExamples::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile << "home.css"
+  config.assets.precompile << "home.css"  
+  config.assets.precompile << "bootstrap-theme.css"
   config.assets.precompile << "examples/*.css"
+  config.assets.precompile << "examples/*.js"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
